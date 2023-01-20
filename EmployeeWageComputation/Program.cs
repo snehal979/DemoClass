f85fbca64f6777003612c87ea12f661b7d11c9d2
Snehal979 <bansodsnehal535@gmail.com>
2023-01-20 09:08:57 +0530

    (SNEHAL)Add: wlecom massage

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome the Employee Wage Computation program");
+            Console.ReadLine();
+        }
+           
+    }
+}
