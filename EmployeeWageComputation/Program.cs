6c1cae47ccbd867bde5dca32d3750ee20d777b88
Snehal979 <bansodsnehal535@gmail.com>
2023-01-20 09:38:58 +0530

    (SNEHAL)Add:modified calculating EmployeeAttendance

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,6 +5,18 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome the Employee Wage Computation program");
+            //UC1 employee attaindance
+            int isPresent = 1;
+            Random random = new Random();
+            int empAttaindance = random.Next(0,2); //0 or 1
+            if(empAttaindance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
            
