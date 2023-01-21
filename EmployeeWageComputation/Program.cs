74b8343548d43dd9ef0642418bfb0b10ec2ac6ee
Snehal979 <bansodsnehal535@gmail.com>
2023-01-21 09:15:09 +0530

    (SNEHAL)Add:part time employee wage using Swich case

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,26 +6,26 @@
         {
             Console.WriteLine("Welcome the Employee Wage Computation program");
             //UC1 employee attaindance
-            int fullTime_isPresent = 1;
-            int partTime_isPresent = 2;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             int empHour = 0;
             int empRatePerHour = 20;
             Random random = new Random();
             int empAttaindance = random.Next(0,3); //0 or 1
-            if(empAttaindance == fullTime_isPresent)
-            {
-                Console.WriteLine("Employee is Full Time Present");
-                empHour = 8;
-
-            } else if (empAttaindance == partTime_isPresent)
-            {
-                Console.WriteLine("Employee is Present Part_Time");
-                empHour = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+           switch(empAttaindance)
+           {
+                case FULL_TIME:
+                    Console.WriteLine("Employee is Full Time Present");
+                    empHour = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("Employee is Present Part_Time");
+                    empHour = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+           }
             int dailyWage = empHour * empRatePerHour;
             Console.WriteLine("Daily Wage for employee is "+dailyWage);
             Console.ReadLine();
