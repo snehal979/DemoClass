133e9041be2bfaae50095c4d208beaaf26980124
Snehal979 <bansodsnehal535@gmail.com>
2023-01-21 08:23:58 +0530

    (Snehal)Add: calculated Daily Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,18 +7,24 @@
             Console.WriteLine("Welcome the Employee Wage Computation program");
             //UC1 employee attaindance
             int isPresent = 1;
+            int fullTimeHour = 0;
+            int empRatePerHour = 20;
             Random random = new Random();
             int empAttaindance = random.Next(0,2); //0 or 1
             if(empAttaindance == isPresent)
             {
                 Console.WriteLine("Employee is Present");
+                fullTimeHour = 8;
+
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            int dailyWage = fullTimeHour*empRatePerHour;
+            Console.WriteLine("Daily Wage for employee is "+dailyWage);
             Console.ReadLine();
         }
            
     }
-}
+} 
