17403b54be2223060134b8defeb1334eb37f747f
Snehal979 <bansodsnehal535@gmail.com>
2023-01-21 09:03:08 +0530

    (SNEHAL)Add:part time employee wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,22 +6,27 @@
         {
             Console.WriteLine("Welcome the Employee Wage Computation program");
             //UC1 employee attaindance
-            int isPresent = 1;
-            int fullTimeHour = 0;
+            int fullTime_isPresent = 1;
+            int partTime_isPresent = 2;
+            int empHour = 0;
             int empRatePerHour = 20;
             Random random = new Random();
-            int empAttaindance = random.Next(0,2); //0 or 1
-            if(empAttaindance == isPresent)
+            int empAttaindance = random.Next(0,3); //0 or 1
+            if(empAttaindance == fullTime_isPresent)
             {
-                Console.WriteLine("Employee is Present");
-                fullTimeHour = 8;
+                Console.WriteLine("Employee is Full Time Present");
+                empHour = 8;
 
+            } else if (empAttaindance == partTime_isPresent)
+            {
+                Console.WriteLine("Employee is Present Part_Time");
+                empHour = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
-            int dailyWage = fullTimeHour*empRatePerHour;
+            int dailyWage = empHour * empRatePerHour;
             Console.WriteLine("Daily Wage for employee is "+dailyWage);
             Console.ReadLine();
         }
